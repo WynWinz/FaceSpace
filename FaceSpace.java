2f06f8af3df22ed9d3ae41e14b5ea1801507e9d1
Wyn Mellett <edm34@pitt.edu>
2016-04-09 13:53:09 -0400

    Initial template

---
+++ FaceSpace.java
@@ -0,0 +1,15 @@
+/*
+ * Authors:
+ * Chris Corsi
+ * Ryan Ching
+ * Edwin Mellett
+ *
+ * Date Started:
+ * 4/9/16
+*/
+
+public class FaceSpace {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+Contributors:
+Chris Corsi
+Ryan Ching
+Edwin Mellett
+
+Project:
+FaceSpace is a group project to make a social application using JDBC and SQL.
