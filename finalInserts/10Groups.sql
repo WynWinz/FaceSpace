921089929d5e0d7cd12073bebc42f5e49c64e4ff
Chris C <chc200@pitt.edu>
2016-04-12 14:07:37 -0400

    finalInsertData

---
+++ finalInserts/10Groups.sql
@@ -0,0 +1,12 @@
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (1,'G1','adipiscing, enim mi tempor lorem,',77,3);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (2,'G2','testing',95,6);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (3,'G3','eu, euismod ac, fermentum vel,',11,11);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (4,'G4','lobortis. Class aptent taciti sociosqu',33,5);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (5,'G5','at pretium aliquet, metus urna',99,4);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (6,'G6','neque sed dictum eleifend, nunc',68,10);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (7,'G7','Cum sociis natoque penatibus et',10,10);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (8,'G8','dolor vitae dolor. Donec fringilla.',15,12);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (9,'G9','eu dui. Cum sociis natoque',81,26);
+INSERT INTO Groups (group_ID,groupName,description,memberLimit,numMembers) VALUES (10,'G10','feugiat nec, diam. Duis mi',27,23);
+
+
