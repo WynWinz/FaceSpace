e9c06f8ef63e2360734efe361a48773ce54f9765
Wyn Mellett <edm34@pitt.edu>
2016-04-12 14:24:27 -0400

    script works except friends

--- addTimestamp.py
+++ addTimestamp.py
@@ -5,7 +5,8 @@ import sys
 import string
 
 inserts = []
-dateAlso = False
+date = False
+time = False
 
 def readFile(fileName):
 	global inserts
@@ -18,17 +19,19 @@ def readFile(fileName):
 
 def addTimestamp():
 	global inserts
-	global dateAlso
+	global date, time
 	temp = []
 	for insert in inserts:
-		data = insert.split(',')
-		timestamp = ' TIMESTAMP '
-		timestamp += data[-1]
-		data[-1] = timestamp
-		if(dateAlso):
-				date = ' DATE '
-				date += data[-2]
-				data[-2] = date
+		data = []
+		if(time):
+			data = insert.split(',')
+			timestamp = ' TIMESTAMP '
+			timestamp += data[-1]
+			data[-1] = timestamp
+		if(date):
+			date = ' DATE '
+			date += data[-2]
+			data[-2] = date
 		temp.append((',').join(data))
 	inserts = temp
 
@@ -40,16 +43,19 @@ def writeFile(fileName):
 	sqlOutput.close()
 
 def main():
-	global dateAlso
-	if(len(sys.argv) is not 4):
-		print("python addTimestamp.py dateAlso <input.sql> <output.sql>")
-		print("dateAlso = 0 if just timestamp dateAlso = 1 if there is date as well")
+	global date, time
+	if(len(sys.argv) is not 5):
+		print("python addTimestamp.py time date <input.sql> <output.sql>")
+		print("date = 0 if no date date = 1 if there is date")
+		print("time = 0 if no timestamp time = 1 if there is timestamp")
 		sys.exit(-1);
 	if(sys.argv[1] == '1'):
-		dateAlso = True
-	readFile(sys.argv[2])
+		time = True
+	if(sys.argv[2] == '1'):
+		date = True
+	readFile(sys.argv[3])
 	addTimestamp()
-	writeFile(sys.argv[3])
+	writeFile(sys.argv[4])
 
 if __name__ == '__main__':
 	main()
